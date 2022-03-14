c6b57b78b71a59621d12b65c3ba5dba7215d0c92
aymantarek16 <aymantarek16@yahoo.com>
2022-03-14 23:43:19 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Js
+let navbar = document.querySelector(".nav").querySelectorAll("a");
+
+// _________________________________________________________
+
+// Jquery
+
+// Loading Page Befor Load Main Page
+$(window).on("load", function () {
+  $(".loader").fadeOut("2000");
+  $(".content").fadeIn("2000");
+});
+
+
+$(document).ready(function () {
+
+  $(".nav a").click(function (e) { 
+      $(".nav a").removeClass("active");
+      $(this).addClass("active");
+  });
+
+
+  
+
+});
+
