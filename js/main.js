1e743639eacd06a1eae727ec39c90e3357f5fc92
aymantarek16 <aymantarek16@yahoo.com>
2022-03-15 20:42:40 +0200

    Add Style Switcher

--- js/main.js
+++ js/main.js
@@ -3,10 +3,59 @@
 /* ========================== Typing animation ========================== */
 var typed = new Typed(".typing", {
   strings: ["Front End", "Web Developer"],
-  typeSpeed: 50,
+  typeSpeed: 60,
   BackSpeed: 60,
   loop: true,
 });
+
+/* ========================== Aside ========================== */
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  allSection = document.querySelectorAll(".section")
+  
+for (let i = 0; i < navList.length; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let i = 0; i < allSection.length; i++) {
+      allSection[i].classList.remove("back-section")
+    }
+    for (let j = 0; j < navList.length; j++) { 
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        allSection[j].classList.add("back-section")
+      }
+      navList[j].querySelector("a").classList.remove("active")
+    }
+    this.classList.add("active");
+    showSection(this)
+    if (window.innerWidth < 1200) {
+      asideSectionTogglerBtn()
+    }
+
+  });
+}
+
+function showSection(elem){
+  for (let i = 0; i < allSection.length; i++) {
+    allSection[i].classList.remove("active")
+  }
+   const target = elem.getAttribute("href").split("#")[1] ;
+   document.querySelector("#"+ target).classList.add("active")
+}
+
+const navTogglerBtn = document.querySelector(".nav-toggler") , 
+      aside = document.querySelector(".aside")
+      navTogglerBtn.addEventListener("click" , () =>{
+        asideSectionTogglerBtn()
+      })
+
+      function asideSectionTogglerBtn(){
+        aside.classList.toggle("open")
+        navTogglerBtn.classList.toggle("open")
+        for (let i = 0; i < allSection.length; i++) {
+          allSection[i].classList.toggle("open")
+          
+        }
+      }
 
 /* ========================== Jquery Code ========================== */
 
@@ -14,12 +63,4 @@ var typed = new Typed(".typing", {
 $(window).on("load", function () {
   $(".loader").fadeOut("2000");
   $(".content").fadeIn("2000");
-});
-
-$(document).ready(function () {
-  // Add Active Class On links In NavBar
-  $(".nav a").click(function (e) {
-    $(".nav a").removeClass("active");
-    $(this).addClass("active");
-  });
 });
