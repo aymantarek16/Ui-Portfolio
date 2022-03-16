092a0d77fcfce9370dc80b28412b964b051f1e65
aymantarek16 <aymantarek16@yahoo.com>
2022-03-16 02:37:04 +0200

    Delete loading logo

--- js/main.js
+++ js/main.js
@@ -11,57 +11,82 @@ var typed = new Typed(".typing", {
 /* ========================== Aside ========================== */
 const nav = document.querySelector(".nav"),
   navList = nav.querySelectorAll("li"),
-  allSection = document.querySelectorAll(".section")
-  
+  allSection = document.querySelectorAll(".section");
+
 for (let i = 0; i < navList.length; i++) {
   const a = navList[i].querySelector("a");
   a.addEventListener("click", function () {
-    for (let i = 0; i < allSection.length; i++) {
-      allSection[i].classList.remove("back-section")
-    }
-    for (let j = 0; j < navList.length; j++) { 
+
+      removeBackSection();
+
+
+
+    for (let j = 0; j < navList.length; j++) {
+
       if (navList[j].querySelector("a").classList.contains("active")) {
-        allSection[j].classList.add("back-section")
+        addBackSection(j)
       }
-      navList[j].querySelector("a").classList.remove("active")
+      navList[j].querySelector("a").classList.remove("active");
     }
     this.classList.add("active");
-    showSection(this)
+    showSection(this);
     if (window.innerWidth < 1200) {
-      asideSectionTogglerBtn()
+      asideSectionTogglerBtn();
     }
-
   });
 }
-
-function showSection(elem){
+function removeBackSection(){
   for (let i = 0; i < allSection.length; i++) {
-    allSection[i].classList.remove("active")
+
+    allSection[i].classList.remove("back-section");
   }
-   const target = elem.getAttribute("href").split("#")[1] ;
-   document.querySelector("#"+ target).classList.add("active")
+}
+function addBackSection(num){
+  allSection[num].classList.add("back-section")
+}
+function showSection(element) {
+  for (let i = 0; i < allSection.length; i++) {
+    allSection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
+function updateNav(element) {
+  for (let i = 0; i < navList.length; i++) {
+    navList[i].querySelector("a").classList.remove("active");
+    const target = element.getAttribute("href").split("#")[1];
+    if (target === navList[i].querySelector('a').getAttribute("href").split('#')[1]) {
+      navList[i].querySelector("a").classList.add("active")
+    }
+  }
+}
+document.querySelector(".hire-me").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index")
+  showSection(this);
+  updateNav(this);
+  removeBackSection()
+  addBackSection(sectionIndex)
+});
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < allSection.length; i++) {
+    allSection[i].classList.toggle("open");
+  }
 }
 
-const navTogglerBtn = document.querySelector(".nav-toggler"), 
-      aside = document.querySelector(".aside")
-      navTogglerBtn.addEventListener("click" , () =>{
-        asideSectionTogglerBtn()
-      })
-
-      function asideSectionTogglerBtn(){
-        aside.classList.toggle("open")
-        navTogglerBtn.classList.toggle("open")
-        for (let i = 0; i < allSection.length; i++) {
-          allSection[i].classList.toggle("open")
-        }
-      }
-
-      /* ========================== Add Wow Animation ========================== */
+/* ========================== Add Wow Animation ========================== */
 
 /* ========================== Jquery Code ========================== */
 
 // Loading Page Befor Load Main Page
-$(window).on("load", function () {
-  $(".loader").fadeOut("2000");
-  $(".content").fadeIn("2000");
-});
+// $(window).on("load", function () {
+//   $(".loader").fadeOut("2000");
+//   $(".content").fadeIn("2000");
+// });
